f9bf3fcbcf17aeb2d2361c3d8f735c3a239e8a79
_ Trentini _ <pietrotrentini5@gmail.com>
2020-11-16 11:05:27 +0100

    Add project files.

---
+++ calc1.0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace calc1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            double a = Operazioni.NumeroDouble();
+            double b = Operazioni.NumeroDouble();
+            double operazione = Operazioni.Scelta(a,b);
+            Operazioni.Stampa(operazione);
+
+        }
+    }
+}
---
+++ operazioni/Operazioni.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace calc1._0
+{
+    public class Operazioni
+    {
+        public static double NumeroDouble()
+        {
+            Console.WriteLine("inserisci un numero: ");
+            double a = double.Parse(Console.ReadLine());
+            return a;
+        }
+        public static double Scelta(double a, double b)
+        {
+            Console.WriteLine("inserisci + per la somma \ninserisci - per la differenza \n" +
+                "inserisci * per la moltiplicazione \ninserisci / per la divisione");
+            string scelta = Console.ReadLine();
+            double risultato = 0;
+            switch (scelta)
+            {
+                case "+":
+
+
+                    risultato = a + b;
+                    break;
+                case "-":
+                    risultato = a - b;
+                    break;
+                case "*":
+                    risultato = a * b;
+                    break;
+                case "/":
+
+                    risultato = a / b;
+                    break;
+                   
+
+            }
+            return risultato;
+            
+        }
+        public static void Stampa(double a)
+        {
+            Console.WriteLine($"Il risultato è {a}");
+        }
+    }
+}
